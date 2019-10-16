ed4fc9fe05976d53104d179c35d74b1bb39b44a3
Brett Porter <gkdesign06@gmail.com>
2019-10-16 01:25:23 -0500

    added script to push data to firebase and started DOM manipulation

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,26 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
+database.ref().on('child_added', function(schedule){
+    trainSchedule = schedule.val()
+    newTrain = $('<tr>');
+    newTrain.append(`<td class="pr-5">${trainSchedule.name}</td><td class="pr-5">${trainSchedule.destination}</td><td class="pr-5">${trainSchedule.frequency}</td>`);
+    $('#train-schedule tbody').append(newTrain);
+})
 
+$('#submit-button').on('click',function(event){
+    event.preventDefault();
+    var trainName = $('#train-name').val().trim();
+    var destination = $('#train-destination').val().trim();
+    var time = $('#train-time').val().trim();
+    var frequency = $('#train-frequency').val().trim();
+    var nextArrival = 
 
-
+    database.ref().push({
+        name:trainName,
+        destination:destination,
+        time:time,
+        frequency:frequency
+    });
+    console.log(trainName);
+})
