467dc374df20a7ada4ec4b5b6b72773bae88dd31
Brett Porter <gkdesign06@gmail.com>
2019-10-16 00:17:13 -0500

    made framework in html and added css styling

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var config = {
+    apiKey: "AIzaSyCZfMqfaQKCltGdZWduip7vZN7wyfty6Lc",
+    authDomain: "train-scheduler-8b233.firebaseapp.com",
+    databaseURL: "https://train-scheduler-8b233.firebaseio.com",
+    projectId: "train-scheduler-8b233",
+    storageBucket: "train-scheduler-8b233.appspot.com",
+    messagingSenderId: "719930280874",
+    appId: "1:719930280874:web:6207b1eea95a57662f39d3"
+  };
+  
+firebase.initializeApp(config);
+var database = firebase.database();
+
+
+
+
