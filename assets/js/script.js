08a8bf44113a90bcb2d4d24a19be66ac15daa039
Brett Porter <gkdesign06@gmail.com>
2020-06-25 13:46:14 -0500

    bug fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var config = {
+const config = {
   apiKey: 'AIzaSyCZfMqfaQKCltGdZWduip7vZN7wyfty6Lc',
   authDomain: 'train-scheduler-8b233.firebaseapp.com',
   databaseURL: 'https://train-scheduler-8b233.firebaseio.com',
@@ -9,45 +9,45 @@ var config = {
 };
 
 firebase.initializeApp(config);
-var database = firebase.database();
+const database = firebase.database();
 
-function reloadTable() {
+const reloadTable = () => {
   $('#train-schedule  tbody').empty();
 
-  database.ref().on('child_added', function (schedule) {
-    var key = schedule.key;
-    var trainSchedule = schedule.val();
-    var timeConverted = moment(trainSchedule.time, 'hh:mm').subtract(
+  database.ref().on('child_added', (schedule) => {
+    const key = schedule.key;
+    const trainSchedule = schedule.val();
+    const timeConverted = moment(trainSchedule.time, 'hh:mm').subtract(
       1,
       'years'
     );
-    var timeDiff = moment().diff(moment(timeConverted), 'minutes');
-    var timeRemaining = timeDiff % trainSchedule.frequency;
-    var minutesAway = trainSchedule.frequency - timeRemaining;
-    var nextArrival = moment().add(minutesAway, 'minutes').format('hh:mm');
+    const timeDiff = moment().diff(moment(timeConverted), 'minutes');
+    const timeRemaining = timeDiff % trainSchedule.frequency;
+    const minutesAway = trainSchedule.frequency - timeRemaining;
+    const nextArrival = moment().add(minutesAway, 'minutes').format('hh:mm');
     newTrain = $('<tr>');
     newTrain.append(
       `<td class="pr-5 the-train">${trainSchedule.name}</td><td class="pr-5">${trainSchedule.destination}</td><td class="pr-5">${trainSchedule.frequency} minutes</td><td class="pr-5">${nextArrival}</td><td class="pr-5">${minutesAway} Minutes away</td><td><button  class="delete-button shadow border-2 border-red-500 hover:bg-red-500 focus:shadow-outline focus:outline-none text-white font-bold py-1 px-2 rounded" data-key="${key}" type="button">delete</button></td>`
     );
     $('#train-schedule tbody').append(newTrain);
   });
-}
-reloadTable();
+};
+
 $('#submit-button').on('click', function (event) {
   event.preventDefault();
-  var trainName = $('#train-name').val().trim();
-  var destination = $('#train-destination').val().trim();
-  var time = $('#train-time').val().trim();
-  var frequency = $('#train-frequency').val().trim();
+  const name = $('#train-name').val().trim();
+  const destination = $('#train-destination').val().trim();
+  const time = $('#train-time').val().trim();
+  const frequency = $('#train-frequency').val().trim();
 
-  if (!trainName || !destination || !time || !frequency) {
+  if (!name || !destination || !time || !frequency) {
     console.error("Form isn't complete");
   } else {
     database.ref().push({
-      name: trainName,
-      destination: destination,
-      time: time,
-      frequency: frequency,
+      name,
+      destination,
+      time,
+      frequency,
       timeAdded: firebase.database.ServerValue.TIMESTAMP,
     });
 
@@ -60,6 +60,9 @@ $('#submit-button').on('click', function (event) {
 
 $(document).on('click', '.delete-button', function () {
   keyref = $(this).attr('data-key');
+  console.log(keyref);
   database.ref().child(keyref).remove();
-  reloadTable();
+  window.location.reload();
 });
+
+reloadTable();
